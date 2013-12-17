3aa0eaae57d41915d54781dcfa010e977500e943
Jonathan Tapia <jonathan.tapia@crowdint.com>
2013-12-16 18:10:38 -0600

    Fix issue with assets in heroku

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module SpreeDemo
     end
 
     config.assets.initialize_on_precompile = true
+
+    config.assets.enabled = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
